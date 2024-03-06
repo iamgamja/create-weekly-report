116efa34e87f7a660fb61d711a234058f1732b3b
iamgamja <hmmthinking@naver.com>
2024-03-06 19:43:11 +0900

    Update fetchMeal.mjs

--- fetchMeal.mjs
+++ fetchMeal.mjs
@@ -1,4 +1,7 @@
-import * as school from "school.hana.js"
+import * as school from 'school.hana.js'
+import { EmbedBuilder, WebhookClient } from 'discord.js'
+
+const webhookClient = new WebhookClient({ url: process.env.DISCORD_WEBHOOK_URL });
 
 const sch = await school.search({
   SCHUL_NM: process.SCHOOL_NAME,
@@ -9,4 +12,10 @@ const res = school.meal({
   
 })
 
-console.log(res)
+const embed = new EmbedBuilder()
+	.setTitle('Some Title')
+	.setColor(0x00FFFF);
+await webhookClient.send({
+  content: res.toString(),
+	embeds: [embed],
+});
