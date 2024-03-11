5b8a65c682355aad4fd33b34ed1664670df97377
iamgamja <hmmthinking@naver.com>
2024-03-11 07:27:33 +0000

    buf to stream

--- sendPng.mjs
+++ sendPng.mjs
@@ -2,12 +2,13 @@ import pdftopic from 'pdftopic'
 import fs from 'fs'
 import fetch from 'node-fetch'
 import formData from 'form-data'
+import { Readable } from 'stream'
 
 const pdf = fs.readFileSync('main.pdf')
-const png = await pdftopic.pdftobuffer(pdf, 0)[0]
+const buf = await pdftopic.pdftobuffer(pdf, 0)[0]
 
 const form = new formData()
-form.append('file1', png)
+form.append('file1', Readable.from(buf))
 
 await fetch(
   process.env.DISCORD_WEBHOOK_URL,
