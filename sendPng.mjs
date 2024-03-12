9ebf15f4cbc58d615cd14318b077d16efb122422
iamgamja <hmmthinking@naver.com>
2024-03-12 23:13:37 +0000

    patch

--- sendPng.mjs
+++ sendPng.mjs
@@ -5,7 +5,7 @@ import { WebhookClient } from 'discord.js'
 const pdf = fs.readFileSync('main.pdf')
 const buf = await pdftopic.pdftobuffer(pdf, 0)[0]
 
-const client = new WebhookClient(process.env.DISCORD_WEBHOOK_URL)
+const client = new WebhookClient({url: process.env.DISCORD_WEBHOOK_URL})
 
 await client.send({
   files: [buf]
