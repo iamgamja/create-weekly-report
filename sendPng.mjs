1ecf9315e294093ddd04cb1d55e7bfcdeaf6c65d
iamgamja <hmmthinking@naver.com>
2024-03-13 08:04:12 +0900

    use discord.js

--- sendPng.mjs
+++ sendPng.mjs
@@ -1,21 +1,12 @@
 import pdftopic from 'pdftopic'
 import fs from 'fs'
-import fetch from 'node-fetch'
-import formData from 'form-data'
+import { WebhookClient } from 'discord.js'
 
 const pdf = fs.readFileSync('main.pdf')
 const buf = await pdftopic.pdftobuffer(pdf, 0)[0]
 
-fs.writeFileSync('main.png', buf)
+const client = new WebhookClient(process.env.DISCORD_WEBHOOK_URL)
 
-const form = new formData()
-form.append('file1', fs.createReadStream('main.png'))
-
-await fetch(
-  process.env.DISCORD_WEBHOOK_URL,
-  {
-    method: 'POST',
-    body: form,
-    headers: form.getHeaders()
-  }
-)
+await client.send({
+  files: [buf]
+})
