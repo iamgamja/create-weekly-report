0e0d709b66363c82e5814a987653d677736de089
iamgamja <hmmthinking@naver.com>
2024-03-13 05:08:57 +0000

    uint8arr to buf

--- sendPng.mjs
+++ sendPng.mjs
@@ -6,7 +6,7 @@ import { WebhookClient } from 'discord.js'
 
 // const pdf = fs.readFileSync('main.pdf')
 // const buf = (await pdftopic.pdftobuffer(pdf, 0))[0]
-const buf = (await pdf2img.convert('main.pdf'))[0]
+const buf = Buffer.from((await pdf2img.convert('main.pdf'))[0])
 console.log(buf)
 
 const client = new WebhookClient({url: process.env.DISCORD_WEBHOOK_URL})
