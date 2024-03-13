5ee44f44f9cbce3692e6b35ee1db910290cf7513
iamgamja <hmmthinking@naver.com>
2024-03-13 05:01:54 +0000

    use pdf-img-convert

--- sendPng.mjs
+++ sendPng.mjs
@@ -1,11 +1,12 @@
 import 'dotenv/config'
-import pdftopic from 'pdftopic'
+// import pdftopic from 'pdftopic'
+import pdf2img from 'pdf-img-convert'
 import fs from 'fs'
 import { WebhookClient } from 'discord.js'
 
-const pdf = fs.readFileSync('main.pdf')
-console.log(pdf)
-const buf = (await pdftopic.pdftobuffer(pdf, 0))[0]
+// const pdf = fs.readFileSync('main.pdf')
+// const buf = (await pdftopic.pdftobuffer(pdf, 0))[0]
+const buf = (await pdf2img.convert('main.pdf'))[0]
 
 const client = new WebhookClient({url: process.env.DISCORD_WEBHOOK_URL})
 
